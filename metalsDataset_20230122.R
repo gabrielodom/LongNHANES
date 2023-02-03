ca378c17899026ba9622eb996bbfc7d593acf9e1
khasa006 <khasa006@fiu.edu>
2023-02-02 21:53:52 -0500

    update blood and urine metals information

--- metalsDataset_20230122.R
+++ metalsDataset_20230122.R
@@ -11,12 +11,17 @@ library(nhanesA)
 
 # Blood Metals ------------------------------------------------------------
 
+#Cadmium, Lead, Mercury, Cotinine & Nutritional Biochemistrie
 bloodMetal_9920 <- nhanes('LAB06')
 bloodMetal_0102 <- nhanes('L06_B') 
+
+# Cadmium, Lead, & Total Mercury - Blood
 bloodMetal_0304 <- nhanes('L06BMT_C')
 bloodMetal_0506 <- nhanes('PBCD_D')
 bloodMetal_0708 <- nhanes('PBCD_E')
 bloodMetal_0910 <- nhanes('PBCD_F')
+
+# Cadmium, Lead, Total Mercury, Selenium, & Manganese - Blood 
 bloodMetal_1112 <- nhanes('PBCD_G')
 bloodMetal_1314 <- nhanes('PBCD_H')
 bloodMetal_1516 <- nhanes('PBCD_I')
@@ -27,12 +32,24 @@ bloodMetal_1718 <- nhanes('PBCD_J')
 
 # Urine Metals ------------------------------------------------------------
 
+# Barium, Beryllium, Cobalt, Cesium, Molybdenum, Lead, Platinum, Antimony, 
+# Thallium, and Tungsten
 urineMetal_9920 <- nhanes('LAB06HM')
+
+# Barium, Beryllium, Cadmium, Cobalt, Cesium, Molybdenum, Lead, Platinum, 
+#Antimony, Thallium, Tungsten, and Uranium
 urineMetal_0102 <- nhanes('L06HM_B')
+
+# beryllium (Be), cobalt (Co), molybdenum (Mo), cadmium (Cd), antimony (Sb), 
+# cesium (Cs), barium (Ba), tungsten (W), platinum (Pt), thallium (TI), 
+# lead (Pb), and uranium (U)
 urineMetal_0304 <- nhanes('L06HM_C')
 urineMetal_0506 <- nhanes('UHM_D')
 urineMetal_0708 <- nhanes('UHM_E')
 urineMetal_0910 <- nhanes('UHM_F')
+
+# Antimony, Arsenic, Barium, Beryllium, Cadmium, Cesium, Cobalt, Lead, Manganese,
+# Molybdenum, Platinum, Strontium, Thallium, Tin, Tungsten, and Uranium
 urineMetal_1112 <- nhanes('UHM_G')
 urineMetal_1314 <- nhanes('UM_H')
 urineMetal_1516 <- nhanes('UM_I')
